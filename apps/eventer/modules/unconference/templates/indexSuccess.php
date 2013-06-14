7286b164d202b505be66ee741c19649b8de42683
Sooricatz <guillaume@toaberlin.com>
2013-06-14 18:06:43 +0200

    Pre styled the unconference part

--- apps/eventer/modules/unconference/templates/indexSuccess.php
+++ apps/eventer/modules/unconference/templates/indexSuccess.php
@@ -5,34 +5,34 @@
 	<div class="textual">
         <p class="lead">A first day, to learn and exchange along the water.</p>
 		<h1>The Unconference<span>&nbsp; August 1, 2013</span></h1>
-        <div class="multicolumn">
-            <p>Nullam quis risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Etiam porta sem malesuada magna mollis euismod. Maecenas sed diam eget risus varius blandit sit amet non magna. Vestibulum id ligula porta felis euismod semper. Maecenas sed diam eget risus varius blandit sit amet non magna. Vestibulum id ligula porta felis euismod semper.</p>
-            <p>Vestibulum id ligula porta felis euismod semper. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas sed diam eget risus varius blandit sit amet non magna. Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-        </div>
-    </div>
     
-    <div class="textual 2013-ed">
-        <h2>Edition 2013</h2>
-        <div id="about-2013">
-            <div id="theme-description">
-                <h3>Reset Tomorrow - An ode to Retrofuturism</h3>
-                <p>Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean lacinia bibendum nulla sed consectetur. Sed posuere consectetur est at lobortis. Vestibulum id ligula porta felis euismod semper.</p>
-                <p>Sed posuere consectetur est at lobortis. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam quis risus eget urna mollis ornare vel eu leo. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        <div id="edition-2013">
+            
+            <div id="about-2013">
+                <h2>Edition 2013 - Reset Tomorrow</h2>
+                <div id="theme-description">
+                    <h3 class="tupper">An ode to Retrofuturism</h3>
+                    <p>Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean lacinia bibendum nulla sed consectetur. Sed posuere consectetur est at lobortis. Vestibulum id ligula porta felis euismod semper.</p>
+                    <p>Sed posuere consectetur est at lobortis. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam quis risus eget urna mollis ornare vel eu leo. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                </div>
+                
+                <div id="sidebar-expect">
+                    <h3 class="tupper">What to expect</h3>
+                    <ul>
+                        <li>Workshops</li>
+                        <li>Talks</li>
+                        <li>Pitch Contests</li>
+                        <li>Art Installation</li>
+                        <li>Live Music & DJ Sets</li>
+                    </ul>
+                </div>
+                
+                <div class="clear"></div>
+            </div>
+        
+            <div id="locations-2013">
             </div>
             
-            <div id="sidebar-expect">
-                <h3>What to expect</h3>
-                <ul>
-                    <li>Workshops</li>
-                    <li>Talks</li>
-                    <li>Pitch Contests</li>
-                    <li>Art Installation</li>
-                    <li>Live Music & DJ Sets</li>
-                </ul>
-            </div>
-        </div>
-    
-        <div id="locations-2013">
         </div>
         
     </div>
