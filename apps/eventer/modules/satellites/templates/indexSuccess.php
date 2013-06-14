52368fceef18b783a9575502449e5ad2d64bbf74
Sooricatz <guillaume@toaberlin.com>
2013-06-14 23:36:16 +0200

    created the page for the satellite event

--- apps/eventer/modules/satellites/templates/indexSuccess.php
+++ apps/eventer/modules/satellites/templates/indexSuccess.php
@@ -3,9 +3,55 @@
 <?php include_partial('hero') ?>
 
 	<div class="textual">
-
-		<h1>The Satellites</h1>
-
-	</div>
+        <h1>The Satellites</h1>
+        <p class="lead">On the second day, TOA becomes a platform for the Berlin ecosystem to build upon.<br />
+        <small>Showcase your product, gather likeminded people over coffe or throw a party. The format is up to you, we help you along the way.</small></p>
+    </div>
+    
+    <div class="textual">
+        <h2>Our partner locations</h2>
+        CAROUSEL AVEC LES ENDROITS
+    </div>
+    
+    <div class="textual">
+        <h2>What to expect</h2>
+        <p><big>Have a look at some ideas below or come up with yours.</big></p>
+        <div class="three-columns first">
+            <h3>Networking</h3>
+            <p>Share memorable moments to connect with your audience!</p>
+            <ul>
+                <li>Breakfast, Lunch or Dinner Meetups</li>
+                <li>Open Office Hours</li>
+                <li>Recruiting Events</li>
+                <li>Cocktails Receptions, Pup Crawls</li>
+                <li>BBQ's, Picknicks</li>
+                <li>Parties</li>
+                <li>...</li>
+            </ul>
+        </div>
+        <div class="three-columns">
+            <h3>Making</h3>
+            <p>When people from different disciplines put their though into action, an innovation is born!</p>
+            <ul>
+                <li>Hackathons</li>
+                <li>DIY Workshops</li>
+                <li>Art Installations</li>
+                <li>Jam Sessions</li>
+                <li>...</li>
+            </ul>
+        </div>
+        <div class="three-columns last">
+            <h3>Learning</h3>
+            <p>Share your knowledge and Karma will be your friend!</p>
+            <ul>
+                <li>Workshops</li>
+                <li>Talks and Panels</li>
+                <li>User Meetups</li>
+                <li>...</li>
+            </ul>
+        </div>
+        <div class="clear"></div>
+        <div class="tcenter"><h3 class="tupper"><big>Your Idea ?</big></h3><a  href="host" class="button_black">Host your own</a></div>
+    </div>
 </div>
 
