693662047b0c463bb750bb1b1affd3ebec7c83a5
Sooricatz <guillaume@toaberlin.com>
2013-06-17 15:47:01 +0200

    Created the pages "become a sponsor" and "contact us"

--- apps/eventer/modules/about/templates/contactSuccess.php
+++ apps/eventer/modules/about/templates/contactSuccess.php
@@ -2,6 +2,46 @@
 
 <?php include_partial('hero') ?>
 
-	<h1>Contact us</h1>
+    <div class="textual">
+	
+        <h1>Tech Open Air Contacts</h1>
+        <h2>Tech Open UG</h2>
+        <p class="lead">
+            Windscheid Str. 18 (zweites Hinterhaus)<br />
+            10627 Berlin<br />
+            <a href="mailto:hello@toaberlin.com">hello@toaberlin.com</a>
+        </p>
 
+        <hr>
+        <div class="threecolumns">
+            <div class="element">
+                <h3>Partners & Sponsors</h3>
+                <h2><small>Maryem El Himani</small></h2>
+                <p>
+                    <a href="mailto:sponsoring@toaberlin.com">sponsoring@toaberlin.com</a><br />
+                    <a href="de.linkedin.com/pub/maryem-el-himani/61/961/326">Linkedin</a>
+                </p>
+            </div>    
+        
+            <div class="element">
+                <h3>Press & Media</h3>
+                <h2><small>Kerstin Bock</small></h2>
+                <p>
+                    (+49) 160-9466-2108<br>
+                    <a href="mailto:press@toaberlin.com" target="_blank">press@toaberlin.com</a><br />
+                    <a href="http://twitter.com/KerstinKolumna" target="_blank">@KerstinKolumna</a><br />
+                    <a href="http://de.linkedin.com/in/kerstinbock" target="_blank">Linkedin</a>
+                </p>
+            </div>
+            
+            <div class="element">
+                <h3>Speakers Relations</h3>
+                <h2><small>Nadine Riede</small></h2>
+                <p>
+                    <a href="speakers@toaberlin.com">speakers@toaberlin.com</a>
+                </p>
+            </div>
+        </div>
+            
+    </div>
 </div>
--- apps/eventer/modules/partners/templates/howSuccess.php
+++ apps/eventer/modules/partners/templates/howSuccess.php
@@ -3,7 +3,85 @@
 <?php include_partial('hero') ?>
 
 	<div class="textual">
-
-		<h1>How and why to become a sponsor</h1>
+        
+        <p class="lead">Be part of something truly unique. It is not just about brand and PR exposure here. Partnering with TOA means truly connecting with the technologists and creative leaders of tomorrow.</p>
+		<h1>Make it happen</h1>
+        <div id="sponsorpackages">
+            <div class="tablecolumn">
+                <h2>Booster</h2>
+                <div class="bundle">€ 3,000</div>
+                <ul>
+                    <li class="item active">Logo on our sponsor page</li>
+                    <li class="item active">Logo on Banners at location</li>
+                    <li class="item active">Newsletter mentioning (more than 3,000 subscribers)</li>
+                    <li class="item active">Thank you mentioning on Twitter &amp; Facebook (more than 8,000 total followers)</li>
+                    <li class="item active">One-year free Job Postings on Jobslike.me</li>
+                    <li class="item inactive">Logo on our promotionnal material (Posters and Apparel)</li>
+                    <li class="item inactive">Mention at opening &amp; end speeches</li>
+                    <li class="item inactive">Exhibition area ("booth") at Kater Holzig (value 4000€)</li>
+                    <li class="item inactive">Logo sign at buffet &amp; bar</li>
+                    <li class="item inactive">Logo on all website pages</li>
+                    <li class="item inactive">Logo on ticketing page</li>
+                    <li class="item inactive">Logo on Video Material</li>
+                    <li class="item inactive">Logo on all stages</li>
+                    <li class="item inactive">Mention in all press releases</li>
+                    <li class="item inactive">Integration in press work</li>
+                    <li><strong><span class="red">2</span> included free tickets (min-value 500€)</strong></li>
+                </ul>
+                <a href="mailto:sponsoring@toaberlin.com" target="_blank" class="button_black button_black_small">Sponsor Us</a>
+            </div>
+        
+            <div class="tablecolumn">
+                <h2>Partner</h2>
+                <div class="bundle">€ 10,000</div>
+                <ul>
+                    <li class="item active">Logo on our sponsor page</li>
+                    <li class="item active">Logo on Banners at location</li>
+                    <li class="item active">Newsletter mentioning (more than 3,000 subscribers)</li>
+                    <li class="item active">Thank you mentioning on Twitter &amp; Facebook (more than 8,000 total followers)</li>
+                    <li class="item active">One-year free Job Postings on Jobslike.me</li>
+                    <li class="item active">Logo on our promotionnal material (Posters and Apparel)</li>
+                    <li class="item active">Mention at opening &amp; end speeches</li>
+                    <li class="item active">Exhibition area ("booth") at Kater Holzig (value 4000€)</li>
+                    <li class="item inactive">Logo sign at buffet &amp; bar</li>
+                    <li class="item inactive">Logo on all website pages</li>
+                    <li class="item inactive">Logo on ticketing page</li>
+                    <li class="item inactive">Logo on Video Material</li>
+                    <li class="item inactive">Logo on all stages</li>
+                    <li class="item inactive">Mention in all press releases</li>
+                    <li class="item inactive">Integration in press work</li>
+                    <li><strong><span class="red">6</span> included free tickets (min-value 1500€)</strong></li>
+                </ul>
+                <a href="mailto:sponsoring@toaberlin.com" target="_blank" class="button_black button_black_small">Sponsor Us</a>
+            </div>
+        
+            <div class="tablecolumn mainbundle">
+                <h2>Patron (max 3)</h2>
+                <div class="bundle">€ 48,000</div>
+                <ul>
+                    <li class="item active">Logo on our sponsor page</li>
+                    <li class="item active">Logo on Banners at location</li>
+                    <li class="item active">Newsletter mentioning (more than 3,000 subscribers)</li>
+                    <li class="item active">Thank you mentioning on Twitter &amp; Facebook (more than 8,000 total followers)</li>
+                    <li class="item active">Your very own Newsletter (more than 3,000 subscribers)</li>
+                    <li class="item active">One-year free Job Postings on Jobslike.me</li>
+                    <li class="item active">Logo on our promotionnal material (Posters and apparel)</li>
+                    <li class="item active">Mention at opening &amp; end speeches</li>
+                    <li class="item active">Exhibition area ("booth") at Kater Holzig (value 4000€)</li>
+                    <li class="item active">Logo sign at buffet &amp; bar</li>
+                    <li class="item active">Logo on all website pages</li>
+                    <li class="item active">Logo on ticketing page</li>
+                    <li class="item active">Logo on Video Material</li>
+                    <li class="item active">Logo on all stages</li>
+                    <li class="item active">Mention in all press releases</li>
+                    <li class="item active">Integration in press work</li>
+                    <li class="item active">… And we organize a satellite event for you</li>
+                    <li><strong><span class="red">15</span> included free tickets (min-value 3750€)</strong></li>
+                </ul>
+                <a href="mailto:sponsoring@toaberlin.com" target="_blank"  class="button_black button_black_small">Sponsor Us</a>
+            </div>
+            <div><p class="lead">Please also <a href="mailto:sponsoring@toaberlin.com" target="_blank">get in touch</a> for custom packages, food, drinks, Wifi, award or party sponsorship opportunities or if you are intersted in bartering a service.</p>
+        </div>
+        </div>
 	</div>
 </div>
