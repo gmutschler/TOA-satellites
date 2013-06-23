eeacdcb92bf3b3f19463bcfb207cebb064ef623a
Sooricatz <guillaume@toaberlin.com>
2013-06-23 22:54:47 +0200

    Change the manage event from the main menu to satellites submenu

--- apps/eventer/modules/satellites/templates/_submenu.php
+++ apps/eventer/modules/satellites/templates/_submenu.php
@@ -1,10 +1,17 @@
-<?php include_partial('global/submenu', array(
+<?php
+
+// Default links
+$links = array(
+    'Introduction'  => 'satellites/index',
+    'Book'          => 'satellites/book',
+    'Host'          => 'satellites/host'
+);
+
+// add hosted link for people who host something
+if($sf_user->isAuthenticated() and count($sf_user->getGuardUser()->getOrganiser()->getEvents())) $links['Manage'] = 'user/hostedevents';
+
+include_partial('global/submenu', array(
 
 	'submenu_title' => 'The Satellites',
-	'links' => array(
-
-		'Introduction'  => 'satellites/index',
-		'Book'          => 'satellites/book',
-		'Host'          => 'satellites/host'
-	)
+	'links' => $links
 )) ?>
--- apps/eventer/templates/layout.php
+++ apps/eventer/templates/layout.php
@@ -33,9 +33,6 @@
                 <li><a href="<?=$sf_request->getUriPrefix().$sf_request->getRelativeUrlRoot().$sf_request->getPathInfoPrefix()?>"<?php if($sf_context->getModuleName() == 'home') { ?> class="selected"<?php } ?>>Home</a></li>
                 <li><a href="<?=url_for('unconference/index')?>"<?php if($sf_context->getModuleName() == 'unconference') { ?> class="selected"<?php } ?>>The Unconference</a></li>
                 <li><a href="<?=url_for('satellites/index')?>"<?php if($sf_context->getModuleName() == 'satellites') { ?> class="selected"<?php } ?>>The Satellites</a></li>
-    <?php if($sf_user->isAuthenticated() and $sf_user->getGuardUser()->getOrganiser() and count($sf_user->getGuardUser()->getOrganiser()->getEvents())): ?>
-                <li><a href="<?=url_for('user/hostedevents')?>"<?php if($sf_context->getModuleName() == 'user') { ?> class="selected"<?php } ?>>Manage Events</a></li>
-    <?php endif ?>
                 <li><a href="<?=url_for('news/index')?>"<?php if($sf_context->getModuleName() == 'news') { ?> class="selected"<?php } ?>>News</a></li>
                 <li><a href="<?=url_for('partners/index')?>"<?php if($sf_context->getModuleName() == 'partners') { ?> class="selected"<?php } ?>>Partners</a></li>
                 <li><a href="<?=url_for('about/index')?>"<?php if($sf_context->getModuleName() == 'about') { ?> class="selected"<?php } ?>>About us</a></li>
