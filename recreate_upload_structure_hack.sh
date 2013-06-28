31d3b754ac4b8810850bf617de400c8adb8fa626
kyeno <maciej@taranienko.pl>
2013-06-29 01:14:25 +0200

    FIXED permissions hack for GitHub

--- recreate_upload_structure_hack.sh
+++ recreate_upload_structure_hack.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+mkdir -p cache
 mkdir -p web/uploads/event_images/pins
 mkdir -p web/uploads/event_images/social_icons
 mkdir -p web/uploads/event_images/thumbs
 mkdir -p web/uploads/organiser_images/
-chmod 777 -R web/uploads
+touch web/css/main.css
+chmod 777 -R cache log web/uploads web/css/main.css
