afe164a24be04ff2f65716870a84e562edaf2566
kyeno <maciej@taranienko.pl>
2013-06-29 02:19:58 +0200

    Another deployment fix

--- recreate_upload_structure_hack.sh
+++ recreate_upload_structure_hack.sh
@@ -5,4 +5,5 @@ mkdir -p web/uploads/event_images/social_icons
 mkdir -p web/uploads/event_images/thumbs
 mkdir -p web/uploads/organiser_images/
 touch web/css/main.css
+touch -d '7 May 2006 14:16' web/css/main.css
 chmod 777 -R cache log web/uploads web/css/main.css
