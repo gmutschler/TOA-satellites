bc22facaa3c99e329d96c5fb34918eb28f20634c
Sooricatz <guillaume@toaberlin.com>
2013-07-05 11:33:37 +0200

    Fixed the the desing of hosted events and change the copy on the buy button in the homepage slider

--- apps/eventer/modules/user/templates/hostedeventsSuccess.php
+++ apps/eventer/modules/user/templates/hostedeventsSuccess.php
@@ -11,25 +11,26 @@
 <?php use_helper('Text') ?>
 
         <div class="listing">
-    
-            <ul class="listing_list">
-    <?php	foreach($events as $event) include_partial('satellites/listing_item', array(
-    
-        'big_link'	=> url_for('satellites/edit?id=' . $event->getId()),
-        'image'		=> $event->getLogo() ? '/uploads/event_images/thumbs/' . $event->getLogo() : '/images/content/test_event_thumb.png',
-        'start_hour'	=> $event->getStartHourClean(),
-        'end_hour'	=> $event->getEndHourClean(),
-        'title'		=> $event->getTitle(),
-        'desc'		=> $event->getDescription(),
-        'org_name'	=> $event->getOrganiser()->getName(),
-        'org_link'	=> $event->getOrganiser()->getUrl(),
-        'ven_name'	=> $event->getVenueName(),
-        'ven_addr'	=> $event->getVenueAddress(),
-        'ven_post'	=> $event->getVenuePostalCode(),
-        'ven_city'	=> $event->getVenueCity(),
-        'color'    => $event->getListingColor()
-    )) ?>
-            </ul>
+            <div class="listing-container">
+                <ul class="listing_list">
+        <?php	foreach($events as $event) include_partial('satellites/listing_item', array(
+        
+            'big_link'	=> url_for('satellites/edit?id=' . $event->getId()),
+            'image'		=> $event->getLogo() ? '/uploads/event_images/thumbs/' . $event->getLogo() : '/images/content/test_event_thumb.png',
+            'start_hour'	=> $event->getStartHourClean(),
+            'end_hour'	=> $event->getEndHourClean(),
+            'title'		=> $event->getTitle(),
+            'desc'		=> $event->getDescription(),
+            'org_name'	=> $event->getOrganiser()->getName(),
+            'org_link'	=> $event->getOrganiser()->getUrl(),
+            'ven_name'	=> $event->getVenueName(),
+            'ven_addr'	=> $event->getVenueAddress(),
+            'ven_post'	=> $event->getVenuePostalCode(),
+            'ven_city'	=> $event->getVenueCity(),
+            'color'    => $event->getListingColor()
+        )) ?>
+                </ul>
+            </div>
     
             <p class="lead tcenter">Click on an event to edit it!</p>
     <?php else: ?>
