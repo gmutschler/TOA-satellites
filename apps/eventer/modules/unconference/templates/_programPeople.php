40c2d197e91ba391993ec34ee4f9ebafc3624a80
Sooricatz <guillaume@toaberlin.com>
2013-07-28 12:34:16 +0200

    added speaker's position and company to program grid

--- apps/eventer/modules/unconference/templates/_programPeople.php
+++ apps/eventer/modules/unconference/templates/_programPeople.php
@@ -1,6 +1,16 @@
 <h3><?=$title?></h3>
 <ul>
 <?php foreach($people as $person): ?>
-	<li><img src="/images/content/speakers-cms/<?=$person->getFace()?>" alt="<?=$person->getFirstName()?> <?=$person->getLastName()?>" /><?=$person->getFirstName()?> <?=$person->getLastName()?></li>
+	<li>
+		<span class="speaker-avatar-container">
+			<?php if($person->getFace()) { ?><img src="/images/content/speakers-cms/<?=$person->getFace()?>" alt="<?=$person->getFirstName()?> <?=$person->getLastName()?>" /><? } ?>
+		</span>
+		<span class="speaker-infos-container">
+            <span class="speaker-name"><?=$person->getFirstName()?> <?=$person->getLastName()?></span>
+			<?php if($person->getCompany()) { ?> <span class="speaker-company"><?=$person->getCompany()?></span><? } ?>
+			<?php if($person->getCompany() & $person->getCompanyPosition()) { ?><span class="speaker-company">&#124;</span><? } ?>
+			<?php if($person->getCompanyPosition()) { ?> <span class="speaker-position"><?=$person->getCompanyPosition()?></span><? } ?>
+		</span>
+	</li>
 <?php endforeach ?>
 </ul>
